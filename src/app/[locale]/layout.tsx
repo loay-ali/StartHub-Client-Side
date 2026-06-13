93e2bdb1b9f89f95bb14c8a9b1ac449e69517f63
Loay Abdelkader <loay.ali.emam.ghonem@gmail.com>
2026-06-13 10:33:18 +0300

    Register Form

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "../dashboard/dashboard.css";
+import "./dashboard/dashboard.css";
 import { NextIntlClientProvider } from "next-intl";
 
 const inter = Inter({
