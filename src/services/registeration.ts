e64e02807332b2b84c508221c66fc9649eb2aa2b
Loay Ali <loay.ali.emam.ghonem@gmail.com>
2026-06-21 18:09:38 +0300

    .

--- src/services/registeration.ts
+++ src/services/registeration.ts
@@ -1,6 +1,30 @@
 import config from "@/constants/config";
+import { RegisterationData } from "@/types/requests/registeration";
+
+export async function init() {
+    const response = await fetch(config.apiUrl +'/registration/register',{
+        method: 'POST',
+        credentials: 'include'
+    });
+
+    console.log(response.status);
+
+    if( response.status == 200 ) {
+        const res = await response.json();
+
+        return res;
+    }
+}
 
 export async function startRegisteration() {
-    const res = fetch(config.apiUrl +'/registeration'
-    )
+    await fetch(config.apiUrl +'/registration/register',{
+        method: 'POST',
+        credentials: 'include'
+    });
+
+    return true;
+}
+
+export async function sendCompanyData(data:RegisterationData) {
+        
 }
